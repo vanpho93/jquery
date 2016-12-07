bd8811bb13391076b77c9b7cabea876d621ee877
Khoa Pham <khoaphp@yahoo.com>
2016-12-07 10:28:57 +0700

    xong api

---
+++ db.js
@@ -0,0 +1,41 @@
+var pg = require('pg');
+var config = {
+  user: 'postgres',
+  password: 'khoapham',
+  host: 'localhost',
+  port: 5432,
+  database: 'EmployeeDB',
+  max: 1000,
+  idleTimeoutMillis: 1000
+}
+
+var pool = new pg.Pool(config);
+
+function queryDB(sql, cb){
+  pool.connect((err, client, done)=> {
+    if(err){
+      console.log('Loi ket noi');
+    }
+    done();
+    client.query(sql, cb);
+  });
+}
+
+function getGirl(id, cb){
+  queryDB(`SELECT * FROM "Girls" WHERE id = ${id}`, (err, result) => {
+    cb(result.rows[0]);
+  });
+}
+
+function likeGirl(id, cb){
+  queryDB(`
+    WITH rows AS
+      (
+      	UPDATE "Girls" SET nlike = nlike + 1 WHERE id = ${id} RETURNING *
+      )
+    SELECT * FROM rows`, (err, result) => {
+      cb(result.rows[0].nlike);
+    });
+}
+
+likeGirl(1, nlike => console.log(nlike));
--- index.js
+++ index.js
@@ -5,4 +5,18 @@ app.set('views', './views');
 app.use(express.static('public'));
 app.listen(3000, () => console.log('Server started'));
 
-app.get('/', (req, res) => res.render('home'));
+var parser = require('body-parser').urlencoded({extended: false});
+
+app.get('/', (req, res) => res.render('home2'));
+
+app.get('/about', (req, res) => res.send('Khoa Pham training'))
+
+app.get('/chao/:ten', (req, res) => {
+  var {ten} = req.params;
+  res.send(`Chao ban ${ten}`);
+});
+
+app.post('/hello', parser, (req, res) => {
+  var {ten, tuoi} = req.body;
+  res.send(`Chao ban ${ten}: ${tuoi} tuoi`);
+});
